6f3d0c9f4f2147d114a4c6ea54da11ad26e08325
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2021-07-10 15:10:33 +0100

    porftolio start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react'
 
 function App() {
-  React.useEffect(() => {
-    const getData = async () => {
-      const res = await fetch('/api/endpoint') // * <-- replace with your endpoint
-      const data = await res.json()
-      console.log(data)
-    }
-    getData()
-  })
-
   return <h1>Hello World</h1>
 }
 
