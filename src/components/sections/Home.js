745a9dc92654953f012ef7fff80c02ea14f756f4
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2021-07-12 15:19:06 +0100

    foundations laid down

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -4,8 +4,24 @@ function Home() {
   return (
     <>
       <div className="title-card">
-        <p>Guy Kozlovskij</p>
-        <p>Full-Stack Developer</p>
+        <p className="name">Guy</p>
+        <p className="surname">Kozlovskij</p>
+        <p className="position">Full-Stack Developer</p>
+      </div>
+      <div className="about">
+        <h1>About Here</h1>
+      </div>
+      <div className="scroll-img-1"></div>
+      <div className="projects">
+        <h1>Projects Here</h1>
+      </div>
+      <div className="scroll-img-2"></div>
+      <div className="experience">
+        <h1>Experience Here</h1>
+      </div>
+      <div className="scroll-img-3"></div>
+      <div className="contact">
+        <h1>Contact Here</h1>
       </div>
     </>
   )
